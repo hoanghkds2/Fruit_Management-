546a9fca18c12d1d99263bd302920ef6e251305b
hoanghkds2 <130424886+hoanghkds2@users.noreply.github.com>
2023-09-22 08:06:08 +0700

    Add files via upload

---
+++ Fruit_Management/src/view/Main.java
@@ -0,0 +1,30 @@
+package view;
+
+import controller.ManageFruitShop;
+import controller.Validation;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ManageFruitShop mg = new ManageFruitShop();
+        while(true){
+        System.out.println("1. Create fruit ");
+        System.out.println("2. View order");
+        System.out.println("3. Shopping ");
+        System.out.println("4. Exit");
+        int choice = sc.nextInt();
+        switch(choice){
+            case 1: mg.createProduct();
+                break;
+            case 2: mg.viewOrders();
+                break;
+            case 3:
+                mg.shopping();
+            case 4: 
+                break;
+        }
+    }
+}
+}
